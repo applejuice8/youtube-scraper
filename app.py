40a771b4e39d188dfc1001c931a8e1c711a9b8a1
Colin <138344363+applejuice8@users.noreply.github.com>
2025-06-14 16:10:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file, redirect, request, url_for
+from flask import Flask, render_template, send_file, request
 from scraper import Scraper
 
 app = Flask(__name__)
